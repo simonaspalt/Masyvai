197020da20daf89e98c247c9c1420a75f58769a8
simonaspalt <simaspa@gmail.com>
2024-03-18 14:52:45 +0200

    2b

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,52 @@ public class Main {
             int num = (int) (Math.round((Math.random() * 20)) + 5);
             array[i] = num;
         }
-        for (int i = 0; i < array.length ; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+        System.out.println();
         System.out.println("====== 2 ============================");
+//Naudodamiesi 1 uždavinio masyvu:
+//a Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
+//b Raskite didžiausią masyvo reikšmę ir jos indeksą arba indeksus jeigu yra keli ir atspausdinkite.
+//c Suskaičiuokite visų porinių (lyginių) indeksų reikšmių sumą;
+//d Sukurkite naują masyvą, kurio reikšmės yra 1 uždavinio masyvo reikšmes minus tos reikšmės indeksas; PVZ -
+// Indeksai   0, 1, 2, 3
+// Reiksmes 1, 17, 40, 3
+// Rezultatas 1, 16, 38, 0
+//e Papildykite masyvą papildomais 10 elementų su reikšmėmis nuo 5 iki 25,
+// kad bendras masyvas padidėtų iki indekso 39; (tam reikės sukurti naują masyvą,
+// ir baigus jį pildyti priskirti pirmojo kintamąjam)
+//g Iš masyvo elementų sukurkite du naujus masyvus. Vienas turi būti sudarytas iš neporinių indekso reikšmių, o kitas iš porinių;
+// Pirminio masyvo elementus su poriniais indeksais padarykite lygius 0 jeigu jie didesni už 15;
+//h Suraskite pirmą (mažiausią) indeksą, kurio elemento reikšmė didesnė už 10;
+        System.out.println("2a");
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ( array[i] > 10){
+                counter++;
+            }
+        }
+        System.out.println("there are: " + counter + " numbers > 10 in array");
+        System.out.println("2b");
+      //b Raskite didžiausią masyvo reikšmę ir jos indeksą arba indeksus jeigu yra keli ir atspausdinkite.
+        int fattestnum = array[0];
+        for (int i = 0; i < (array.length - 1); i++) {
+           if (array[i] > array[( i + 1 )]){
+               fattestnum = array[i];
+           }
+           if (array[i + 1] >array[i]){
+               fattestnum = array[i + 1];
+           }
+        }
+        for (int i = 0; i < array.length ; i++) {
+            if (fattestnum == array[i]){
+                System.out.println("largest number is: " + fattestnum + " with index: " + i);
+            }
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("2b");
+//Suskaičiuokite visų porinių (lyginių) indeksų reikšmių sumą;
 
 
 
