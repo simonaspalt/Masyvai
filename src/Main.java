ea8c6f9c6ddc2046b1442dca2c5a88411e30dfe4
simonaspalt <simaspa@gmail.com>
2024-03-18 14:05:16 +0200

    1 uzd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29),
+        // kurių reikšmės yra atsitiktiniai skaičiai nuo 5 iki 25.
+        // Atsispausdinate, pasižiūrite ką gavote.
+        System.out.println("====== 1 ============================");
+        int[] array = new int[30];
+        for (int i = 0; i < 30; i++) {
+            int num = (int) (Math.round((Math.random() * 20)) + 5);
+            array[i] = num;
+        }
+        for (int i = 0; i < array.length ; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("====== 2 ============================");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
